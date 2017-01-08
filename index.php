4c38faa00c67f4fb9a796eeb35f72ae9ab2bf935
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-08 04:25:02 +0000

    flex header, footer, body

--- index.php
+++ index.php
@@ -3,26 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Calendar</title>
+        <title>Flex-This</title>
         <meta name="description" content="calendar">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <div class="container">
+            <header>
+                <h1>Main Page</h1>
+            </header>
 
-        <nav>
-            <ul>
-                <li class="nav-item"><a href="#">Home</a></li>
-                <li class="nav-item"><a href="#">About</a></li>
-                <li class="nav-item"><a href="#">Forum</a></li>
-                <li class="nav-item"><a href="#">Support</a></li>
-            </ul>
-        </nav>
+            <main>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </main>
 
-        <div>
-            
+            <footer>
+                <h5>Footer</h5>
+                <p>Some extra notes and such.</p>
+            </footer>
         </div>
-
     </body>
 </html>
